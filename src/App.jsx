f2f3aaad74233ad178ab9361214b618518525321
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-06-07 18:01:19 +0200

    commit register and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Login } from "./pages/Login/login.page";
+import { Register } from "./pages/Register/register.page";
 import "./App.css";
 
 function App() {
-    return <></>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/register" element={<Register />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/Login/Login.page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+const Login = () => {
+    return (
+        <Flex gap="small" wrap>
+            <Button type="primary">Primary Button</Button>
+        </Flex>
+    );
+};
+
+export { Login };
