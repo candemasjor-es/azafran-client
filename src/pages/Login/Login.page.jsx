af138c65bd7368393b8df2390137f08d4186c3f0
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-06-09 12:00:24 +0200

    commit

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,8 +1,56 @@
-import { Button } from "antd";
+import { useState } from "react";
+import { Button, Flex, Input, Typography } from "antd";
+const { Title } = Typography;
+
 const Login = () => {
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+
+    const handleLoginButtonClick = () => {
+        console.log(user, password);
+        fetch("http://localhost:8080/login/", {
+            headers: {
+                "Content-type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify({ username: user, password: password }),
+        })
+            .then(async (res) => {
+                const data = await res.json();
+                if (res.status >= 400 && data.msg) {
+                    setErrorMsg(data.msg);
+                } else {
+                    localStorage.setItem("access_token", data.accessToken);
+                }
+            })
+            .catch((err) => {
+                console.warn(err);
+            });
+    };
+
     return (
-        <Flex gap="small" wrap>
-            <Button type="primary">Primary Button</Button>
+        <Flex gap={"8px"} vertical style={{ width: "400px" }}>
+            <Title>Login</Title>
+            <Input
+                value={user}
+                onChange={(event) => setUser(event.target.value)}
+                placeholder="Usuario"
+            />
+            <Input
+                placeholder="Contraseña"
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+            />
+            <Button
+                type="primary"
+                disabled={!user.trim() || !password.trim()}
+                onClick={handleLoginButtonClick}
+            >
+                Log in
+            </Button>
+            <Typography.Text>{errorMsg}</Typography.Text>
         </Flex>
     );
 };
