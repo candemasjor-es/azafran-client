394e5b6fa96aba542b6314b0d7e8cf742d4358b7
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-06-12 10:11:17 +0200

    commit

--- src/routes/UnAuthedRoutes.jsx
+++ src/routes/UnAuthedRoutes.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes, Navigate } from "react-router";
-import { Login } from "../pages/Login/login.page";
+import { Login } from "../pages/Login/Login.page";
 import { Register } from "../pages/Register/Register.page";
 
 const UnAuthedRoutes = () => {
